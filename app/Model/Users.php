7102bd36132a635787b271b6145e176ee49b9753
ciara <ciara@localhost>
2017-03-31 16:41:16 +0800

    show blog list and admin route

--- app/Model/Users.php
+++ app/Model/Users.php
@@ -3,11 +3,18 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Authenticatable; 
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
 class Users extends Model implements AuthenticatableContract,
                                     AuthorizableContract,
                                     CanResetPasswordContract
 {
+    use Authenticatable, Authorizable, CanResetPassword;
     /**
      * The database table used by the model.
      *
